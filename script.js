672be18eac531f16619a49a1d1527fb3d453b686
niranj775 <niranj775@gmail.com>
2025-02-13 18:54:20 +0530

    Final App

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let userInput = document.getElementById('birthdate');
-userInput.max
+userInput.max = new Date().toISOString().split('T')[0];
 
 function calculateAge(){
+    let birthdate = new Date(userInput.value);
+    let currentDate = new Date();
+    let age = currentDate.getFullYear() - birthdate.getFullYear();
+    let month = currentDate.getMonth() - birthdate.getMonth();
+    console.log(age,month);
+    if (month < 0 || (month === 0 && currentDate.getDate() < birthdate.getDate())) {
+        age--;
+    }
+    document.getElementById('result').innerHTML = `You are <span>${age}</span> years old.`;
 
 }
